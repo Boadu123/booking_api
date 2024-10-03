0d1474ed0d2708f03196446d603d540f32f3f3ea
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2024-10-03 15:20:23 +0000

    changes made

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from "express";
 import { bookingRouter } from "./routes/all-routes.js";
 import mongoose from "mongoose";
 import "dotenv/config";
+import cors from "cors"
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(bookingRouter); 
 
